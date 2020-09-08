2da91f128fd52e74166017ef8180e43bba7da81e
skyoneking <741567163@qq.com>
2020-09-08 17:34:01 +0800

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws')
+const {object2queryString, getQueryObject} = require('./utils')
+
+const url = 'ws://localhost:3040?' + object2queryString({
+    wsType: 'server',
+    path: 'test'
+})
+
+const ws = new WebSocket(url)
+ws.on('open', () => {
+    console.log('server连接成功')
+    ws.send('hahaha')
+})
+
+ws.on('message', data => {
+    console.log(data)
+})
+
+ws.on('error', e => {
+    console.log(e)
+})
